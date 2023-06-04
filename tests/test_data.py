c1a3ca37bd261f9046fb6097cc4377b248702149
Victoria Kashpur <vikki0011@gmail.com>
2023-06-04 18:42:03 +1000

    fix improve tests for utils & data

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,5 +5,5 @@ def test_get_data(result_test_get_data):
     assert get_data('test_operations.json') == result_test_get_data
 
 
-def test_sort_data(result_test_sort_data):
-    assert sort_data(get_data('test_operations.json')) == result_test_sort_data
+def test_sort_data(data_for_sort, result_test_sort_data):
+    assert sort_data(data_for_sort) == result_test_sort_data
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,9 +2,9 @@ from src.utils import get_operations, encode_numbers
 from src.data import get_data, sort_data
 
 
-def test_get_operations(result_test_get_operations):
-    assert get_operations(sort_data(get_data('test_operations.json'))) == result_test_get_operations
+def test_get_operations(result_test_sort_data, result_test_get_operations):
+    assert get_operations(result_test_sort_data) == result_test_get_operations
 
 
-def test_encode_numbers(result_test_encode_numbers):
-    assert encode_numbers(get_operations(sort_data(get_data('test_operations.json')))) == result_test_encode_numbers
+def test_encode_numbers(result_test_get_operations, result_test_encode_numbers):
+    assert encode_numbers(result_test_get_operations) == result_test_encode_numbers
