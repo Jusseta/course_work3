44237e50efc2abc7b229c8558b825e3ac7244266
Victoria Kashpur <vikki0011@gmail.com>
2023-06-04 14:38:50 +1000

    add data

---
+++ src/data.py
@@ -0,0 +1,16 @@
+import json
+
+
+def get_data(json_file):
+    with open(json_file, encoding='utf-8') as file:
+        all_data = json.loads(file.read())
+        return all_data
+
+
+def sort_data(data):
+    full_data = []
+    for i in data:
+        if 'state' in i:
+            if i['state'] == "EXECUTED":
+                full_data.append(i)
+    return sorted(full_data, key=lambda k: k['date'].split('.'), reverse=True)
